07b0b4b129cae8592614268b73b319288c04432c
olteanuflorin86 <olteanuflorin86@gmail.com>
2021-03-07 20:53:13 +0200

    Installed firebase dependencies and initial firebase setup in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+import {useAuthState} from 'react-firebase-hooks/auth';
+
+firebase.initializeApp ({
+  // your config
+  // First we'll create a firebase project at the firebase website
+})
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
 
 const App = () => {
   return(
-    <div>
-      <h1>App</h1>
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
     </div>
   )
 }
